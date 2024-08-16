33cd4f0813767a376add696bffdef1b54324f466
iPadla <petrozavr@ya.ru>
2024-08-16 20:05:05 +0300

    :(

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,9 +1,9 @@
 let counter = function () {
-  if (+document.getElementById("timer") < 0) {
+  let timer = document.getElementById("timer");
+  timer.textContent--;
+  if (timer.textContent < 0) {
     alert("Вы победили в конкурсе");
     clearInterval(counter);
   }
 };
-setInterval(counter, 500);
-
-document.addEventListener("DOMContentLoaded", counter());
+setInterval(counter, 100);
